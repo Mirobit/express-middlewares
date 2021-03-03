315eeba4bf9b4f11e4bf2402d69ec2925473e12e
Michael <13236924+Mirobit@users.noreply.github.com>
2021-03-04 00:40:05 +0100

    Use esm instead of cjs

--- middlewares/security.js
+++ middlewares/security.js
@@ -5,8 +5,11 @@ const security = (req, res, next) => {
   res.header("X-Content-Type-Options", "nosniff");
   res.header("Referrer-Policy", "same-origin");
   res.header("X-XSS-Protection", "1; mode=block");
-  res.header("Content-Security-Policy", "default-src 'self'; object-src 'none'");
+  res.header(
+    "Content-Security-Policy",
+    "default-src 'self'; object-src 'none'"
+  );
   next();
 };
 
-module.exports = security;
+export default security;
